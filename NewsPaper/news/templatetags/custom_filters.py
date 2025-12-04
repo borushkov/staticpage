bb099f0c0368ab1c8dc375c6585bffb9ee80b2bc
Gleb <glebborushkov06041@gmail.com>
2025-12-04 18:48:50 +0300

    Initial commit

--- NewsPaper/news/templatetags/custom_filters.py
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -2,20 +2,17 @@ from django import template
 
 register = template.Library()
 
-# Список слов для цензуры (лучше использовать константу в верхнем регистре)
-CENSOR_WORDS = ['текст', 'второй', 'редиска', 'плохой']  # ← исправлено имя
+
+CENSOR_WORDS = ['текст', 'второй', 'редиска', 'плохой']  
 
 @register.filter
 def censor(value):
-    """
-    Фильтр для цензурирования слов.
-    """
-    # Проверяем, что значение - строка
+  
     if not isinstance(value, str):
         return value
     
     result = value.split()
-    # Проходим по всем словам для цензуры
+ 
     for k in range(len(result)):
             if result[k].lower() in CENSOR_WORDS:
                 result[k] = result[k][0] + ('*'* (len(result[k])-1))
